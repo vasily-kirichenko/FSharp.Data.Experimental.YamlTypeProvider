6a3d0d97286951b7b3b00858b95f3bbed6254081
vasily-kirichenko <vaskir@gmail.com>
2013-11-30 13:35:16 +0400

    add separate runtime project

--- src/FSharp.Data.YamlTypeProvider/YamlTypeProvider.fs
+++ src/FSharp.Data.YamlTypeProvider/YamlTypeProvider.fs
@@ -1,4 +1,4 @@
-﻿namespace FSharp.Data.Experimental.YamlTypeProvider
+﻿namespace FSharp.Data.Experimental
 
 open System.Reflection
 open Microsoft.FSharp.Core.CompilerServices
@@ -8,30 +8,10 @@ open Microsoft.FSharp.Quotations
 open System.IO
 open System.Collections.Generic
 
-module Yaml =
-    open YamlDotNet.Core
-    open YamlDotNet.RepresentationModel
+module TypesFactory =
+    open Yaml
 
-    type Scalar =
-        | Int of int
-        | String of string
-        | TimeSpan of TimeSpan
-        | Bool of bool
-        static member Parse (value: string) =
-            match bool.TryParse value with
-            | true, x -> Bool x
-            | _ ->
-                match Int32.TryParse value with
-                | true, x -> Int x
-                | _ -> match TimeSpan.TryParse value with
-                       | true, x -> TimeSpan x
-                       | _ -> String value
-        member x.UnderlyingType = 
-            match x with
-            | Int x -> x.GetType()
-            | String x -> x.GetType()
-            | Bool x -> x.GetType()
-            | TimeSpan x -> x.GetType()
+    type Scalar with
         member x.ToExpr() = 
             match x with
             | Int x -> Expr.Value x
@@ -41,30 +21,6 @@ module Yaml =
                 let parse = typeof<TimeSpan>.GetMethod("Parse", [|typeof<string>|])
                 Expr.Call(parse, [Expr.Value (x.ToString())])
 
-    type Node =
-        | Scalar of Scalar
-        | List of Node list
-        | Map of (string * Node) list
-
-    let parse text =
-        let rec loop (n: YamlNode) =
-            match n with
-            | :? YamlScalarNode as n -> Scalar (Scalar.Parse n.Value)
-            | :? YamlSequenceNode as n -> List (n.Children |> Seq.map loop |> Seq.toList)
-            | :? YamlMappingNode as n -> 
-                Map (n.Children |> Seq.choose (fun p -> 
-                    match p.Key with
-                    | :? YamlScalarNode as keyNode -> Some (keyNode.Value, loop p.Value)
-                    | _ -> None) |> Seq.toList)
-            | x -> failwithf "Unsupported YAML node type: %s" (x.GetType().Name)
-
-        let stream = YamlStream()
-        use reader = new StringReader(text)
-        stream.Load(reader)
-        let doc = stream.Documents.[0]
-        loop doc.RootNode
-
-module TypesFactory =
     type T =
         { MainType: Type option
           Types: MemberInfo list
@@ -146,15 +102,6 @@ module TypesFactory =
               Types = [mapTy :> MemberInfo; field :> MemberInfo; prop :> MemberInfo]
               Init = fun me -> Expr.FieldSet(me, field, Expr.NewObject(ctr, [])) }
         | None -> { MainType = None; Types = childTypes; Init = childInits }
-
-type Root(filePath: string) = 
-    member x.Save (stream: Stream) =
-        let serializer = YamlDotNet.RepresentationModel.Serialization.Serializer()
-        use writer = new StreamWriter(stream)
-        serializer.Serialize(writer, x)
-    member x.Save() =
-        use file = new FileStream(filePath, FileMode.Create)
-        x.Save file
 
 [<TypeProvider>]
 type public YamlProvider (cfg: TypeProviderConfig) as this =
