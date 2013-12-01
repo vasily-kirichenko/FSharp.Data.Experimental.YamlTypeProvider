fd74fb75fabd0754b6f187f4b4c2f2e28e77178a
vasily-kirichenko <vaskir@gmail.com>
2013-12-01 16:45:26 +0400

    add ChangeSettings.fsx

---
+++ examples/ChangeSettings.fsx
@@ -0,0 +1,9 @@
+﻿#r @"Settings\bin\Debug\FSharp.Data.YamlTypeProvider.dll"
+#r @"Settings\bin\Debug\Settings.dll"
+
+open Settings
+
+let settings = Settings()
+settings.DB.ConnectionString <- "new connection string"
+settings.Save (__SOURCE_DIRECTORY__ + @"ChangedSettings.yaml")
+
