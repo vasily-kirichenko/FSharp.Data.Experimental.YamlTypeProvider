cad8b91cb58b32a02a4c2b71a8e0b1b251e531ba
vasily-kirichenko <vaskir@gmail.com>
2013-12-01 21:16:32 +0400

    playing with SharpYaml

--- examples/ChangeSettings.fsx
+++ examples/ChangeSettings.fsx
@@ -1,9 +1,13 @@
 ﻿#r @"Settings\bin\Debug\FSharp.Data.YamlTypeProvider.dll"
 #r @"Settings\bin\Debug\Settings.dll"
 
-open Settings
+open System
 
-let settings = Settings()
+let settings = Settings.Settings()
+
+settings.Mail.Pop3.Port <- 400
 settings.DB.ConnectionString <- "new connection string"
-settings.Save (__SOURCE_DIRECTORY__ + @"ChangedSettings.yaml")
+settings.DB.DefaultTimeout <- TimeSpan.FromMinutes 30.
+
+settings.Save (__SOURCE_DIRECTORY__ + @"\ChangedSettings.yaml")
 
---
+++ examples/Script.fsx
@@ -0,0 +1,18 @@
+﻿#r @"..\packages\SharpYaml.1.1.0\lib\SharpYaml.dll"
+#r @"Settings\bin\Debug\FSharp.Data.YamlTypeProvider.dll"
+#r @"Settings\bin\Debug\Settings.dll"
+
+open System
+open SharpYaml
+open SharpYaml.Serialization
+
+let settings = Settings.Settings()
+settings.Mail.Smtp.Ssl <- false
+settings.ToString()
+
+let st = SerializerSettings(EmitDefaultValues=true)
+st.EmitAlias <- false
+st.SpecialCollectionMember <- ""
+let sr = Serializer(st)
+let s = sr.Serialize settings
+s
